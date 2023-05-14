a22418a138bb6ea98ad100761bdf38557a5fe7bf
trtobeha <trtobeha@gmail.com>
2023-05-14 16:19:52 +0300

    Add models to reviews

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ...
+
+
+class Category(models.Model):
+    ...
+
+
+class Genre(models.Model):
+    ...
+
+
+class Title(models.Model):
+    ...
+
+
+class Review(models.Model):
+    ...
+
+
+class Comment(models.Model):
+    ...
